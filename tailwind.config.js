eb08260adf3fd1b6017ce55d545d576195331cbe
ramliagustiawan <ramliagustiawan@gmail.com>
2023-01-28 14:14:19 +0800

    menit 1:37 section klien yg pernah kerjasama

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary:'#14b8a6',
-      }
+      },
+      screens: {
+        '2xl':'1440px'
+      },
     },
   },
   plugins: [],
